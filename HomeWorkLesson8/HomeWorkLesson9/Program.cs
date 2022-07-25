a57cd3c54183c2b6d35f9710c911d717a54097e3
Valery <pandasuit7@gmail.com>
2022-07-25 13:00:35 +0300

    HW

---
+++ HomeWorkLesson8/HomeWorkLesson9/Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N, кратные трём.
+
+void Zadacha64()
+{
+    Console.WriteLine("Введите число m");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число n");
+    int n = Convert.ToInt32(Console.ReadLine());
+    AllNumber(m, n);
+    
+}
+
+void AllNumber(int m, int n)
+{
+    if (m > n)
+    {
+        return;
+    }
+    if (m % 3 == 0)
+    {
+        Console.WriteLine(m + "\t");
+    }
+    m++;
+    AllNumber(m, n);
+}
+//Zadacha64();
+//Zadacha66();
+
+
+// Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+void Zadacha66()
+{
+    Console.WriteLine("Введите число m");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число n");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Sum(m, n);
+    
+    
+}
+
+void Sum(int m, int n, int sum=0)
+{
+    if (m > n)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + m;
+    m++;
+    Sum(m, n, sum);
+
+}
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+Console.WriteLine("Введите число m");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число n");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(AkkermanFunc(m, n));
+
+
+int AkkermanFunc(int m, int n)
+{
+   if (m == 0)
+   {
+       return n + 1;
+   }
+   else if (n == 0)
+   {
+       return AkkermanFunc(m - 1,1);
+   }
+   else 
+   {
+       return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+   }
+}
---
+++ HomeWorkLesson8/Program.cs
@@ -0,0 +1,151 @@
+﻿//Zadacha54();
+//Zadacha56();
+Zadacha58();
+
+// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
+
+void Zadacha54()
+{
+    int rows = 4;
+    int columns = 5;
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    Console.WriteLine("отсортированный массив:");
+    
+for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - 1 - j; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+    }
+    PrintArray(array);
+    
+    }
+
+
+
+
+
+void FillArray(int[,] array)
+{
+    {
+            Random random = new Random();
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    array[i, j] = random.Next(1, 10);
+                }
+            }
+
+        }
+}
+
+
+
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+  for (int i = 0; i < rows; i++)
+  {
+      for (int j = 0; j < columns; j++)
+      {
+          Console.Write(array[i, j] + "\t");
+      }
+      Console.WriteLine();
+  }  
+}
+
+
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void Zadacha56()
+{
+    int rows = 4;
+    int columns = 5;
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    int sumMinIndex = 0;
+    
+
+
+
+
+    int minSum = 0;
+    for (int j = 0; j < columns; j++)
+    {
+      minSum += array[0, j];
+    }
+    Console.WriteLine($"Сумма первой строки равна {minSum}");
+    for (int i = 1; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма чисел: {sum}, в строке {i+1}");
+        if (sum < minSum)
+        {
+            minSum = sum;
+            sumMinIndex = i;
+            
+        }
+        
+    }
+    Console.WriteLine($"Минимальная сумма находится в строке {sumMinIndex} {minSum}");
+}
+
+//Заполните спирально массив 4 на 4.
+
+void Zadacha58()
+{
+    int rows = 6;
+    int columns = 6;
+    int[,] array = new int[rows, columns];
+    int indexI = 0;
+    int indexJ = 0;
+    int changeI = 0;
+    int changeJ = 1;
+    int Step = columns;
+    int turn = 0;
+    for (int i = 1; i < array.Length + 1; i++)
+    {
+        array[indexI, indexJ] = i;
+        
+        Step --;
+        if (Step == 0)
+        {
+           Step = columns * (turn%2) + rows * ((turn + 1)%2) - 1 - turn/2;
+           int temp = changeI;
+           changeI = changeJ;
+           changeJ = -temp;
+           turn ++;
+        }
+        
+        indexI += changeI;
+        indexJ += changeJ;
+    }
+Console.WriteLine();
+PrintArray(array);
+}
